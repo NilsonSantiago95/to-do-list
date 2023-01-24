0bfd0298b9f3cbfa8f9a3082803732ac49ff79ff
Nilson Santiago <nilson.santiago19@gmail.com>
2023-01-24 20:46:33 -0300

    Aula Dados através das rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,14 +1,25 @@
 const express = require('express')
 
+const Checklist = require('../models/checklist')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log('ola')
-    res.send()
+router.get('/', async (req, res) => {
+    try {
+        let checklists = await Checklist.find({})
+        res.status(200).render('checklists/index', { checklists: checklists })
+    } catch(error) {
+        res.status(200).render('pages/error', {error: 'Erro ao exibir as listas'})
+    }
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.body.params)
+        res.status(200).render('checklists/show', { checklist: checklist })
+    } catch(error) {
+        res.status(200).render('pages/error', {error: 'Erro ao exibir as listas de tarefas'})
+    }
 })
 
 router.post('/', (req, res) => {
