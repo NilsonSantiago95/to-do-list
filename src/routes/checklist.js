5b3c09fe66999871b2c2f0dea1b032b4948562e9
Nilson Santiago <nilson.santiago19@gmail.com>
2023-01-26 20:44:23 -0300

    Aprendendo a receber  dados do formulário

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -13,6 +13,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/new', async (req, res) => {
+    try {
+        let checklist = new Checklist()
+        res.status(200).render('checklists/new', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao carregar o formulário' })
+    }
+})
+
 router.get('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.body.params)
@@ -22,8 +31,16 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    let { name } = req.body.checklist
+    let checklist = new Checklist({name})
 
+    try {
+        await checklist.save()
+        res.redirect('/checklists')
+    } catch (error) {
+        res.status(422).render('checklists/new', {checklists: {...checklist, error}})
+    }
 })
 
 router.put('/:id', (req, res) => {
